ba9fb27aef9c32962dc7d18ff4d30fcefbf30071
Papun <Papunkumardas0@gmail.com>
2022-10-14 16:22:09 +0530

    initial

---
+++ target/META-INF/mule-src/firebase-connector/src/main/java/net/info/firebaseconnector/internal/FirebaseOperations.java
@@ -0,0 +1,39 @@
+package net.info.firebaseconnector.internal;
+
+import static org.mule.runtime.extension.api.annotation.param.MediaType.ANY;
+
+import org.mule.runtime.extension.api.annotation.param.MediaType;
+import org.mule.runtime.extension.api.annotation.param.Config;
+import org.mule.runtime.extension.api.annotation.param.Connection;
+
+
+/**
+ * This class is a container for operations, every public method in this class will be taken as an extension operation.
+ */
+public class FirebaseOperations {
+
+  /**
+   * Example of an operation that uses the configuration and a connection instance to perform some action.
+   */
+  @MediaType(value = ANY, strict = false)
+  public String retrieveInfo(@Config FirebaseConfiguration configuration, @Connection FirebaseConnection connection){
+    return "Using Configuration [" + configuration.getConfigId() + "] with Connection id [" + connection.getId() + "]";
+  }
+
+  /**
+   * Example of a simple operation that receives a string parameter and returns a new string message that will be set on the payload.
+   */
+  @MediaType(value = ANY, strict = false)
+  public String Select(String url,String key) {
+    return buildHelloMessage(url,key);
+  }
+
+  /**
+   * Private Methods are not exposed as operations
+   */
+  private String buildHelloMessage(String url,String key) {
+	  
+	 //logic here 
+    return " ";
+  }
+}
